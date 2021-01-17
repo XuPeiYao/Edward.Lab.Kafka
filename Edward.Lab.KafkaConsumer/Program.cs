f46a28030b9a0ef9b31cc735964d4f145e342847
Edward Hsu <st89382000@gmail.com>
2021-01-17 18:25:25 +0800

    加入專案檔案。

---
+++ Edward.Lab.KafkaConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Edward.Lab.KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
