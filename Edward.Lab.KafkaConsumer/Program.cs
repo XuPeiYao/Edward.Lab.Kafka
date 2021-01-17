1959b7b590453cd6bb43062626abec202221dc0b
Edward Hsu <st89382000@gmail.com>
2021-01-17 18:54:34 +0800

    feat: Implement Kafka producer and consumer use Confluent.Kafka package

--- Edward.Lab.KafkaConsumer/Program.cs
+++ Edward.Lab.KafkaConsumer/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using Confluent.Kafka;
+
+using System;
+using System.Threading;
 
 namespace Edward.Lab.KafkaConsumer
 {
     class Program
     {
+        const string TOPIC_NAME = "my-test-topic";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var conf = new ConsumerConfig
+            {
+                GroupId = nameof(Edward.Lab.KafkaConsumer),
+                BootstrapServers = "kafka:9092",
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // earliest message in the topic 'my-topic' the first time you run the program.
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                consumer.Subscribe(TOPIC_NAME);
+
+                while (true)
+                {
+                    try
+                    {
+                        var cr = consumer.Consume(cts.Token);
+
+                        if (cr == null) continue;
+
+                        Console.WriteLine($"Message: {cr.Message.Value}");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Error: " + e);
+                    }
+                }
+            }
+
+            Console.Read();
         }
     }
 }
--- Edward.Lab.KafkaProducer/Program.cs
+++ Edward.Lab.KafkaProducer/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Confluent.Kafka;
+
+using System;
+using System.Threading;
 
 namespace Edward.Lab.KafkaProducer
 {
     class Program
     {
+        const string TOPIC_NAME = "my-test-topic";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var conf = new ProducerConfig { BootstrapServers = "kafka:9092" };
+
+            using (var p = new ProducerBuilder<Null, string>(conf).Build())
+            {
+                int i = 0;
+                while (true)
+                {
+                    p.Produce(TOPIC_NAME, new Message<Null, string> { Value = "MESSAGE-" + i++.ToString() });
+                    Console.WriteLine("Sended Message " + i);
+                    Thread.Sleep(1000);
+                }
+
+                // wait for up to 10 seconds for any inflight messages to be delivered.
+                p.Flush(TimeSpan.FromSeconds(10));
+            }
+
+            Console.Read();
         }
     }
 }
